7c58af1f0086bf307628d5d504b8bcbfce216547
Gertjan Leemans <gertjan@safeguardapp.nl>
2020-05-12 20:44:04 +0200

    Update issue-types.js

--- src/issue-types.js
+++ src/issue-types.js
@@ -8,16 +8,51 @@ module.exports = [
         title: 'maintenance',
         userFriendly: '🧰 Maintenance',
         filter: '(^maintenance)',
-        order: 3
+        order: 9
     }, {
         title: 'feature',
         userFriendly: '🚀 Features',
         filter: '(^feature)',
-        order: 0
+        order: 1
     }, {
         title: 'fix',
         userFriendly: '🐛 Bug Fixes',
         filter: '(^fix)',
-        order: 1
+        order: 2
+    }, {
+        title: 'hotfix',
+        userFriendly: '🚑 Hotfixes',
+        filter: '(^hotfix)',
+        order: 0
+    }, {
+        title: 'docs',
+        userFriendly: '💡 Docs',
+        filter: '(^docs)',
+        order: 8
+    }, {
+        title: 'style',
+        userFriendly: '💄 Styling',
+        filter: '(^style)',
+        order: 7
+    }, {
+        title: 'refactor',
+        userFriendly: '🎨 Refactoring',
+        filter: '(^refactor)',
+        order: 4
+    }, {
+        title: 'performance',
+        userFriendly: '⚡️ Performance improvements',
+        filter: '(^performance)',
+        order: 3
+    }, {
+        title: 'test',
+        userFriendly: '✅ Tests',
+        filter: '(^test)',
+        order: 5
+    }, {
+        title: 'chore',
+        userFriendly: '👷‍ Chores',
+        filter: '(^chore)',
+        order: 6
     }
 ];
